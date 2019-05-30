a781d0b39911ac7fedfcfa5f2ceb74626b3bea62
san7557 <hadigi7557@gmail.com>
2019-05-30 17:25:26 +0300

    не знаю, шо происходит,но коммичу, шобы не сломалосб

--- Qt/Lab_4/main.cpp
+++ Qt/Lab_4/main.cpp
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <openssl/evp.h>
-
-
-using namespace std;
+#include <string>
+#include <cstring>
 
 #define BUFSIZE 1024 //Любое число байт, кратное 16.
 
@@ -16,20 +15,17 @@ void printCharsAsHex(unsigned char *buf, int len){
 }
 
 //General encryption and decryption function example using FILE I/O and AES256 with a 256-bit key.
-int do_crypt(int do_encrypt){
+int do_crypt(char* textin, char* textout, int do_encrypt){
     unsigned char inbuf[BUFSIZE], outbuf[BUFSIZE + EVP_MAX_BLOCK_LENGTH]; //EVP_MAX_BLOCK_LENGTH = 128 бит
     int inlen, outlen;
 
-    scanf("%s", inbuf);
-    inlen = sizeof(inbuf);
-    //strcpy((char*)inbuf -> *textin.c_str());
-   // reinterpret_cast<char>(inbuf[BUFSIZE]);
-    //gets(inbuf[BUFSIZE]);
-    //cout<<inbuf<<"\n";
+    std::cout<<textin<<"\n";
 
-    //unsigned char *text1 = inbuf;
-    //char * convetrted = reinterpret_cast<char*>(text1);
-    //gets(convetrted);
+    inlen = strlen(textin);
+    std::cout<<inlen<<"\n";
+
+    for(int i = 0; i < inlen; i++)
+        inbuf[i] = textin[i];
 
     unsigned char key[] = "0123456789abcdeF0123456789abcdeF"; //256 бит
     unsigned char iv[] = "1234567887654321"; //128 бит
@@ -42,30 +38,28 @@ int do_crypt(int do_encrypt){
     EVP_CipherInit_ex(ctx, NULL, NULL, key, iv, do_encrypt);
 
     //for(;;){
-        //inlen = read(inbuf, 1, BUFSIZE, in);
-        printf("%d B, In:\n", inlen);
-        //if (inlen <= 0) break;
-        if(!EVP_CipherUpdate(ctx, outbuf, &outlen, inbuf, inlen)){
-            EVP_CIPHER_CTX_free(ctx);
-            //return 0;
-        }
-        //fwrite(outbuf, 1, outlen, out);
-        printf("  %d B, OutU:\n", outlen);
-        //cout<<inbuf<<"\n";
-        //printCharsAsHex(outbuf, outlen);
+    //inlen = fread(inbuf, 1, BUFSIZE, in);
+    printf("%d B, In:\n", inlen);
+    //if (inlen <= 0) break;
+    if(!EVP_CipherUpdate(ctx, outbuf, &outlen, inbuf, inlen)){
+        EVP_CIPHER_CTX_free(ctx);
+        return 0;
+    }
+    //fwrite(outbuf, 1, outlen, out);
+    printf("  %d B, OutU:\n", outlen);
+    //printCharsAsHex(outbuf, outlen);
     //}
 
     if(!EVP_CipherFinal_ex(ctx, outbuf, &outlen)){
         EVP_CIPHER_CTX_free(ctx);
-        //return 0;
+        return 0;
     }
     //fwrite(outbuf, 1, outlen, out);
     printf("  %d B, OutF:\n", outlen);
-    cout<<inbuf<<"\n"<<outbuf<<"\n";
     //printCharsAsHex(outbuf, outlen);
 
     EVP_CIPHER_CTX_free(ctx);
-    //return 1;
+    return 1;
 }
 
 int main(int argc, char *argv[]){
@@ -73,10 +67,26 @@ int main(int argc, char *argv[]){
 
     //############
 
+    std::string text1;
+    std::cin>>text1;
+    int size = text1.length();
+    std::cout<<size<<"\n";
+    char* text = new char[size];
+    for (int i = 0; i < size; i++)
+    {
+        text[i] = text1[i];
+        std::cout<<text[i];
+    }
+    std::cout<<"\n";
+    std::cout<<text;
+
+    char* textenc = new char[size+16];
+    char* textdec = new char[size];
+
     printf("ENCRYPT:\n\n");
     //FILE *encode_file = fopen("D:\\181_331_vasyutkin\\vasyutkin_term2\\Qt\\Lab_4\\file.txt", "rb");
     //FILE *decode_file = fopen("D:\\181_331_vasyutkin\\vasyutkin_term2\\Qt\\Lab_4\\file1.txt", "wb");
-    do_crypt(1); // 0 - decrypt, 1 - encrypt
+    do_crypt(text, textenc, 1); // 0 - decrypt, 1 - encrypt
     //fclose(encode_file);
     //fclose(decode_file);
 
@@ -85,7 +95,7 @@ int main(int argc, char *argv[]){
 
     //encode_file = fopen("D:\\181_331_vasyutkin\\vasyutkin_term2\\Qt\\Lab_4\\file1.txt", "rb");
     //decode_file = fopen("D:\\181_331_vasyutkin\\vasyutkin_term2\\Qt\\Lab_4\\file2.txt", "wb");
-    do_crypt(0); // 0 - decrypt, 1 - encrypt
+   // do_crypt(*textenc, textdec, 0); // 0 - decrypt, 1 - encrypt
     //fclose(encode_file);
     //fclose(decode_file);
 
