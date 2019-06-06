0d6db6725c66e35953630d1c4aba95f1390b0ecb
san7557 <hadigi7557@gmail.com>
2019-06-06 18:05:57 +0300

    ну наконец-то шифрование стало похоже на норм прошу

--- Qt/Lab_4/main.cpp
+++ Qt/Lab_4/main.cpp
@@ -4,7 +4,6 @@
 #include <QTextStream>
 #include <string.h>
 #include <stdio.h>
-//#include <unistd.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/conf.h>
@@ -15,60 +14,66 @@
 
 using namespace std;
 
-
-int do_crypt(unsigned char *plaintext, unsigned char *ciphertext, int do_encrypt)
+int do_crypt(unsigned char *sourcetext, unsigned char *ciphertext, int do_encrypt)
 {
-    EVP_CIPHER_CTX *ctx;
-    int plaintext_len = strlen((char *)plaintext), len, ciphertext_len;
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int sourcetext_len = strlen((char *)sourcetext),
+            len,
+            ciphertext_len;
+    unsigned char *key = (unsigned char *)"0123456789abcdeF0123456789abcdeF",
+            *iv = (unsigned char *)"1234567887654321";
 
-    if(!(ctx = EVP_CIPHER_CTX_new()))
+    if(!EVP_CipherInit_ex(ctx, EVP_aes_256_cbc(), NULL, NULL, NULL, do_encrypt))
+    {
+        EVP_CIPHER_CTX_free(ctx);
         return 0;
+    }
 
-    unsigned char *key = (unsigned char *)"0123456789abcdeF0123456789abcdeF";
-    unsigned char *iv = (unsigned char *)"1234567887654321";
+    OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 32);
+    OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == 16);
 
-    if(1 != EVP_CipherInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv, do_encrypt))
+    if(!EVP_CipherInit_ex(ctx, NULL, NULL, key, iv, do_encrypt))
+    {
+        EVP_CIPHER_CTX_free(ctx);
         return 0;
+    }
 
-    if(1 != EVP_CipherUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+    if(!EVP_CipherUpdate(ctx, ciphertext, &len, sourcetext, sourcetext_len))
+    {
+        EVP_CIPHER_CTX_free(ctx);
         return 0;
+    }
     ciphertext_len = len;
 
-    if(1 != EVP_CipherFinal_ex(ctx, ciphertext + len, &len))
+    if(!EVP_CipherFinal_ex(ctx, ciphertext + len, &len))
+    {
+        EVP_CIPHER_CTX_free(ctx);
         return 0;
+    }
     ciphertext_len += len;
 
-    /* Clean up */
     EVP_CIPHER_CTX_free(ctx);
     ciphertext[ciphertext_len] = '\0';
     return ciphertext_len;
 }
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     QCoreApplication a(argc, argv);
 
-    //###########
+    string stringtext;
+    getline(cin, stringtext);
+    unsigned char *sourcetext = (unsigned char*)stringtext.c_str(),
+            ciphertext[128],
+            decryptedtext[128];
 
-        string stringtext;
-        getline(cin, stringtext);
-        unsigned char *plaintext = (unsigned char*)stringtext.c_str();
+    do_crypt(sourcetext, ciphertext, 1);
+    printf("Ciphertext is:\n");
+    printf("%s\n", ciphertext);
 
-        unsigned char ciphertext[128];
-        unsigned char decryptedtext[128];
-
-        do_crypt(plaintext, ciphertext, 1);
-
-        printf("Ciphertext is:\n");
-        printf("%s\n", ciphertext);
-
-        do_crypt(ciphertext, decryptedtext, 0);
-
-
-        printf("Decrypted text is:\n");
-        printf("%s\n", decryptedtext);
-
-    //############
+    do_crypt(ciphertext, decryptedtext, 0);
+    printf("Decrypted text is:\n");
+    printf("%s\n", decryptedtext);
 
     return a.exec();
 }
